10d2538e3346aa5d37a3774772e83556ab2b93ca
ConstaChugga <constachugga@gmail.com>
2020-03-29 11:44:13 -0700

    Add files via upload

--- LIP.lua
+++ LIP.lua
@@ -1,16 +1,16 @@
 --[[
 	Copyright (c) 2012 Carreras Nicolas
-	
+
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
-	
+
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
-	
+
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,19 +22,16 @@
 --- Lua INI Parser.
 -- It has never been that simple to use INI files with Lua.
 --@author Dynodzzo
+---Modified by ConstaChymic
 
 local LIP = {};
 
---- Returns a table containing all the data from the INI file.
---@param fileName The name of the INI file to parse. [string]
---@return The table containing all data from the INI file. [table]
-function LIP.load(fileName)
-	assert(type(fileName) == 'string', 'Parameter "fileName" must be a string.');
-	local file = assert(io.open(fileName, 'r'), 'Error loading file : ' .. fileName);
+--does the actual work of converting the data to a table
+local function convertToTable (iterator)
 	local data = {};
 	local section;
-	for line in file:lines() do
-		local tempSection = line:match('^%[([^%[%]]+)%]$');
+	for line in iterator do
+		local tempSection = line:match('^%[([^%[%]]*)%]$');
 		if(tempSection)then
 			section = tonumber(tempSection) and tonumber(tempSection) or tempSection;
 			data[section] = data[section] or {};
@@ -54,27 +51,65 @@ function LIP.load(fileName)
 			data[section][param] = value;
 		end
 	end
+	return data;
+end
+
+--- Returns a table containing all the data from the INI file.
+--@param fileName The name of the INI file to parse. [string]
+--@return The table containing all data from the INI file. [table]
+function LIP.load(fileName)
+	assert(type(fileName) == 'string', 'Parameter "fileName" must be a string.');
+	local file = assert(io.open(fileName, 'r'), 'Error loading file : ' .. fileName);
+	local data = convertToTable(file:lines())
 	file:close();
 	return data;
+end
+
+--- Returns a table containing all the data from the INI file.
+--- Same as load, but you just pass the file directly rather than the path.
+--@param fileName The name of the INI file to parse. [string]
+--@return The table containing all data from the INI file. [table]
+function LIP.loadFile(file)
+	local f = file:lines()
+	local data = convertToTable(f)
+	file:close()
+	return data;
+end
+
+--- Returns a table containing all the data from the INI file.
+--- Same as load, except takes an ini string rather than a file
+--@param iniString The INI file as a string to load. [string]
+--@return The table containing all data from the INI file. [table]
+function LIP.loadString(iniString)
+	assert(type(iniString) == 'string', 'Parameter "iniString" must be a string.');
+	iniString = iniString:gsub("\r\n", "\n");
+	return convertToTable(string.gmatch(iniString, "([^\n]+)"))
+end
+
+--- Converts the given table to an INI string
+--@param data The table containing all the data to store. [table]
+function LIP.buildString(data, isfile)
+    assert(type(data) == 'table', 'Parameter "data" must be a table.');
+    local rline = isfile and '\r\n' or '\n'
+    local contents = '';
+    for section, param in pairs(data) do
+        contents = contents .. ('[%s]\n'):format(section);
+        for key, value in pairs(param) do
+            contents = contents .. ('%s=%s\n'):format(key, tostring(value));
+        end
+        contents = contents .. rline;
+    end
+    return contents;
 end
 
 --- Saves all the data from a table to an INI file.
 --@param fileName The name of the INI file to fill. [string]
 --@param data The table containing all the data to store. [table]
 function LIP.save(fileName, data)
-	assert(type(fileName) == 'string', 'Parameter "fileName" must be a string.');
-	assert(type(data) == 'table', 'Parameter "data" must be a table.');
-	local file = assert(io.open(fileName, 'w+b'), 'Error loading file :' .. fileName);
-	local contents = '';
-	for section, param in pairs(data) do
-		contents = contents .. ('[%s]\n'):format(section);
-		for key, value in pairs(param) do
-			contents = contents .. ('%s=%s\n'):format(key, tostring(value));
-		end
-		contents = contents .. '\n';
-	end
-	file:write(contents);
-	file:close();
+    assert(type(fileName) == 'string', 'Parameter "fileName" must be a string.');
+    local file = assert(io.open(fileName, 'w+b'), 'Error loading file :' .. fileName);
+    file:write(LIP.buildString(data, true));
+    file:close();
 end
 
 return LIP;
